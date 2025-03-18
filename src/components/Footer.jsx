03f2ee76a9da1ed43879031981b3b75f4b51c6e8
Raul <luis_1547@hotmail.com>
2025-03-18 00:37:38 -0600

    integrated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+
+
+<div>
+  <footer className="main-footer">
+    <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div className="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.1.0
+    </div>
+  </footer>
+  {/* Control Sidebar */}
+  <aside className="control-sidebar control-sidebar-dark">
+    {/* Control sidebar content goes here */}
+  </aside>
+</div>
+
+    </div>
   )
 }
