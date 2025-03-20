c5b8afb26352ccf68e02e0a58ef53fe5ada00b75
Raul <luis_1547@hotmail.com>
2025-03-20 02:01:43 -0600

    changes on actors and address , and adding constant to replace all urls

---
+++ src/routes/routes.js
@@ -0,0 +1,27 @@
+ var routes = [
+    
+    {
+        name : 'Dashboard',
+        url : '/',
+        icon: 'far fa-circle nav-icon"'
+    },
+
+    {
+        name : 'Actor',
+        url : '/actor',
+        icon:'far fa-circle nav-icon"'
+    },
+
+    {
+        name : 'Film',
+        url : '/film',
+        icon:'far fa-circle nav-icon"'
+    },
+
+    {
+        name : 'Address',
+        url : '/address',
+        icon:'far fa-circle nav-icon"'
+    },
+];
+export default routes;
